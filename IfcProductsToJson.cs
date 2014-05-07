dc1c5a685d10596d442a295d37a4b54708c9758c
yemaw <yemaw.online@gmail.com>
2014-05-07 18:37:35 +0800

    added summary and product info

--- IfcProductsToJson.cs
+++ IfcProductsToJson.cs
@@ -22,6 +22,7 @@ using Xbim.Ifc2x3.PropertyResource;
 using Xbim.Ifc2x3.RepresentationResource;
 
 using System.Runtime.Serialization;
+using System.Collections.Specialized;
 //using System.Web.Script.Serialization.JavaScriptSerializer;
 //using Newtonsoft.Json;
 /*
@@ -38,18 +39,32 @@ namespace XBIMConsole
     {
         public void writeProducts(XbimModel model) {
             
-            //
             Dictionary<string, string> header = new Dictionary<string, string>();
-            List<Dictionary<string, Dictionary<string, Dictionary<string, string>>>> data = new List<Dictionary<string, Dictionary<string, Dictionary<string, string>>>>();
-            
+            List<object> data = new List<object>(); //Dictionary<string, Dictionary<string, Dictionary<string, string>>> //old type
+            Dictionary<string, object> output = new Dictionary<string, object>();
+
             IEnumerable<IfcProduct> products = model.Instances.OfType<IfcProduct>();
-            
             int products_counter = 0;
             foreach (IfcProduct product in products)
             {
-                Dictionary<string, Dictionary<string, Dictionary<string, string>>> product_d = new Dictionary<string, Dictionary<string, Dictionary<string, string>>>();
-                
-                //Properties
+                Dictionary<string, object> product_d = new Dictionary<string, object>();//Dictionary<string, Dictionary<string, string>>//old type
+
+                //1)Product's Information
+                product_d.Add("name", product.Name);
+                product_d.Add("ifctype", product.GetType());
+                product_d.Add("description", product.Description);
+                product_d.Add("global_id", product.GlobalId);
+
+                //2)Product's Summary
+                Dictionary<string, object> summaries_d = new Dictionary<string, object>();
+                StringCollection summaries = product.SummaryString();
+                summaries_d.Add("entity", summaries[0]);
+                summaries_d.Add("guid", summaries[1]);
+                summaries_d.Add("type", summaries[2]);
+                summaries_d.Add("name", summaries[3]);
+                product_d.Add("summary", summaries_d);
+
+                //3)Product's Properties
                 Dictionary<string, Dictionary<string, string>> props_set_d = new Dictionary<string, Dictionary<string, string>>();
                 List<IfcPropertySet> sets = product.GetAllPropertySets();
                 foreach (IfcPropertySet set in sets)
@@ -64,7 +79,7 @@ namespace XBIMConsole
                     if (props_set_d.ContainsKey(set.Name.ToString())) //mearge if already exist.
                     { 
                         foreach (KeyValuePair<string, string> entry in props_d)
-                        {
+                        {//pset_d.ToList().ForEach(x => product_d[set.Name].Add(x.Key, x.Value));//old way.
                             if (props_set_d.ContainsKey(set.Name))
                             {
                                 props_set_d[set.Name][entry.Key] = entry.Value; //replace
@@ -75,8 +90,6 @@ namespace XBIMConsole
                             }
                             
                         }
-
-                        //pset_d.ToList().ForEach(x => product_d[set.Name].Add(x.Key, x.Value));
                     } 
                     else 
                     {
@@ -84,20 +97,30 @@ namespace XBIMConsole
                     }
                     
                 }
-                
                 product_d.Add("properties_set", props_set_d);
+                
+                
 
-                Console.Write("\rCurrent count - {0}", products_counter);
+                //---------- Insert packed product into ouput body----------------
                 data.Insert(products_counter, product_d);
                 products_counter++;
-                if (products_counter == 100) { break; }
-            }
-            string output = Newtonsoft.Json.JsonConvert.SerializeObject(data);
-            
-            System.IO.File.WriteAllText(Directory.GetCurrentDirectory()+"/output.txt", output);
 
-            //header.Add("project_name", model.IfcProject.Name);
-            //header.Add("products_count", products.Count().ToString());
+                Console.Write("\rCurrent count - {0}", products_counter-1);
+                if (products_counter == 10) { break; }
+            }
+            
+            //--------------------- Packing Project in one object ----------------
+            header.Add("project_name", model.IfcProject.Name);
+            header.Add("project_description", model.IfcProject.Description);
+            header.Add("ifcproducts_count", products.Count().ToString());
+            
+            output.Add("header", header);
+            output.Add("data", data);
+
+            string output_str = Newtonsoft.Json.JsonConvert.SerializeObject(output);
+
+            System.IO.File.WriteAllText(@"C:\Users\yemaw\gelement\sync\ifcproducts.txt", output_str);
+            //System.IO.File.WriteAllText(Directory.GetCurrentDirectory()+"/ifcproducts.txt", output);
         }
     }
 }
